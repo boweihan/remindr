081f40607b2428130ddc65191e125a9c9af6e7cb
Jon Mio <jon.mio@hotmail.com>
2016-07-19 18:09:12 -0400

    api using url

--- googleauth/app/controllers/pages_controller.rb
+++ googleauth/app/controllers/pages_controller.rb
@@ -5,8 +5,10 @@ class PagesController < ApplicationController
     client_secrets = Google::APIClient::ClientSecrets.load
     @auth_client = client_secrets.to_authorization
     @auth_client.update!(
-      :scope => 'https://www.googleapis.com/auth/gmail.readonly '+
-                'https://www.googleapis.com/auth/gmail.send',
+      :scope => 'https://www.googleapis.com/auth/userinfo.email ' +
+      'https://www.googleapis.com/auth/userinfo.profile '+
+      'https://www.googleapis.com/auth/gmail.readonly '+
+      'https://www.googleapis.com/auth/gmail.send',
       :redirect_uri => 'http://localhost:3000/callback'
       )
   end
@@ -21,28 +23,34 @@ class PagesController < ApplicationController
       puts "error"
 
     elsif params[:code]
+      #client accepted scopes. use authorization code in qs
       @auth_client.code = params[:code]
+
+      #exchange auth for token
       @auth_client.fetch_access_token!
-      # Initialize the API
-      service = Google::Apis::GmailV1::GmailService.new
-      service.authorization = @auth_client
-      # Show the user's labels
-      user_id = 'me'
-      result = service.list_user_messages(
-       user_id,
-       max_results: 1,
-       q: 'from:contact@codecademy.com',
-      )
-      result = result.messages
-      result.each do |text|
-       text_body = service.get_user_message(
-         user_id,
-         text.id,
-       )
-       puts (text_body.payload.parts)[1].body.data
-      end
-      puts "success"
-      puts @auth_client.inspect
+      token = @auth_client.access_token
+
+      #get id
+      url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=#{token}"
+      obj = RestClient.get(url)
+      id = JSON.parse(obj)['id']
+
+      q= "from:contact@codecademy.com"
+      #get messages list
+      api_url = "https://www.googleapis.com/gmail/v1/users/#{id}/messages?maxResults=1&q=#{q}&access_token=#{token}"
+      puts api_url
+      email_id = JSON.parse(RestClient.get(api_url))['messages'][0]['id']
+
+      #get 1 mesage
+      email_api_url = "https://www.googleapis.com/gmail/v1/users/#{id}/messages/#{email_id}?access_token=#{token}"
+      puts email_api_url
+      email_obj = RestClient.get(email_api_url)
+      email_hash = JSON.parse(email_obj)
+
+      # 0 text, 1 html
+      email_body = email_hash['payload']['parts'][0]['body']['data']
+      #decode
+      email_body = Base64.decode64(email_body.gsub("-", '+').gsub("_","/"))
     end
 
     redirect_to '/garbage'
