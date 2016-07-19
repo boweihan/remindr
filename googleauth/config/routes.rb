5df88942aa1dbd63ec229ad56a96796932ccb833
Jon Mio <jon.mio@hotmail.com>
2016-07-19 18:35:30 -0400

    removed nseted project

--- googleauth/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  get '/callback', to: 'pages#callback'
-  get '/garbage', to: 'pages#garbage'
-end
