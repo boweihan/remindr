c56d9f0e1a53049f72834cc95bcedb55447e37f8
Jon Mio <jon.mio@hotmail.com>
2016-07-20 17:40:36 -0400

    saved file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,7 @@ class PagesController < ApplicationController
       )
   end
 
+
   def googleauth
       auth_uri = @auth_client.authorization_uri.to_s
       redirect_to auth_uri
@@ -28,8 +29,8 @@ class PagesController < ApplicationController
       @auth_client.code = params[:code]
       #exchange auth for token
       @auth_client.fetch_access_token!
-      current_user.update_attributes({access_token: @auth_client.access_token, refresh_token: @auth_client.refresh_token})
-      current_user.get_email(current_user.access_token)
+      token = @auth_client.access_token
+      refresh_token = @auth_client.refresh_token
     end
     redirect_to '/newsfeed'
   end
@@ -46,6 +47,15 @@ class PagesController < ApplicationController
       @contacts << Contact.find(message.contact_id)
       @messages << message
     end
+
+
+    #give the newsfeed the ability to send gmail messages
+    # gmail_send_url = "https://www.googleapis.com/upload/gmail/v1/users/#{current_user.google_id}/messages/send?uploadType=media&access_token=#{current_user.access_token}"
+    #
+    # obj = RestClient.post(gmail_send_url,
+    #                       {Host: 'www.googleapis.com', contentType: 'message/rfc822'},
+    #                       raw: "Q29udGVudC1UeXBlOiB0ZXh0L3BsYWluOyBjaGFyc2V0PSJ1cy1hc2NpaSINCk1JTUUtVmVyc2lvbjogMS4wDQpDb250ZW50LVRyYW5zZmVyLUVuY29kaW5nOiA3Yml0DQp0bzogYnJ5Y2VAdGhvcm1lZGlhLmNvbQ0KZnJvbTogYnJ5Y2VAdGhvcm1lZGlhLmNvbQ0Kc3ViamVjdDogVGVzdA0KDQpIZWxsbyE=")
+    # @sent_message = JSON.parse(obj)
 
   end
 
