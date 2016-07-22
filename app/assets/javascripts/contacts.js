f36115737ff3044eff2c435e4bdef74a8af56761
Carol Yao <c.y-@hotmail.com>
2016-07-21 22:58:23 -0400

    styled the modal box for adding contact

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -7,4 +7,14 @@ $( function() {
   $('.add-icon').on('click', function() {
     $('.modal').css('display', 'block')
   })
+
+  $('.modal').on('click', function() {
+    $('.modal').css('display', 'none')
+  });
+
+  $('.field').on('click', function(e) {
+    e.stopPropagation();
+    $(this).css('border', '1px solid green')
+  });
+
   })
