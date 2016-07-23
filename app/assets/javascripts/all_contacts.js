2070f91095d2d1935a77ed649e61763490ab67d9
Carol Yao <c.y-@hotmail.com>
2016-07-23 19:07:12 -0400

    fixed handlebars bug to display lists of contacts

--- app/assets/javascripts/all_contacts.js
+++ app/assets/javascripts/all_contacts.js
@@ -8,19 +8,11 @@ $(function() {
         data: {},
         dataType: "json"
       }).done(function(responseData) {
-
-        console.log(responseData);
-
         var source = $('#contact-template').html();
-        console.log(source);
-
         var template = Handlebars.compile(source);
 
         var output = template(responseData);
-        console.log(output)
-
         $('.contacts-area').html(output);
-
 
       }).fail(function() {
         console.log('Request failed')
