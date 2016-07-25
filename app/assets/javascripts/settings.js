83041e5a169f042259cce132dc8e02156bb2aae9
elizabeth <elizabeth.e.epstein@gmail.com>
2016-07-25 11:10:15 -0400

    resolved edit user information modal problems, changed route of menu icon from dashboard-path to newsfeed-path

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -4,17 +4,12 @@ $( function() {
     $('.modal2').css('display', 'none')
   });
 
-  $('.settings-icon').on('click', function() {
+  $('#nav-icon').on('click', function() {
     $('.modal2').css('display', 'block')
   });
 
   $('.modal2').on('click', function() {
     $('.modal2').css('display', 'none')
   });
-
-  // $('.field').on('click', function(e) {
-  //   e.stopPropagation();
-  //   $(this).css('border', '1px solid green')
-  // });
 
 })
