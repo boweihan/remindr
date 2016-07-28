503ce223a856aa52575b77f96959a5744f7bd773
Carol Yao <c.y-@hotmail.com>
2016-07-28 19:09:34 -0400

    fixed modal

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -2,5 +2,11 @@ $(function() {
   $('.signup').on('click', function(e) {
     e.preventDefault();
     $('.sign-up-modal').css('display', 'block');
-  })
+  });
+
+  $('.close').on('click', function(e) {
+    $('.sign-up-modal').css('display', 'block');
+  });
+
+
 })
