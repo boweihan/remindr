21dba3938c2e1e8814d4d42838270dff4e9faf2d
Jon Mio <jon.mio@hotmail.com>
2016-07-28 19:30:48 -0400

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,12 @@
 # Contact.create(name: 'Adrian', phone: '9999999999', email: 'bad',user_id:1, category: 'family')
 # Contact.create(name: 'Adrian', phone: '9999999999', user_id:1, category: 'family')
 
-Contact.create(name: 'Ella', phone: '9999999999', email: 'ella@bitmaker.co', user_id:1, category: 'business', twitter: '1967372929', twitter_username: 'BoweiHan')
-Contact.create(name: 'Erik', phone: '9999999999', email: 'erik@bitmakerlabs.com' ,user_id:1, category: 'business', twitter: '1942442', twitter_username: 'BoweiHan')
-Contact.create(name: 'Erik Invoices', phone: '9999999999', email: 'erik@bitmaker.co',user_id:1, category: 'business', twitter: '1967377642', twitter_username: 'BoweiHan')
-Contact.create(name: 'Adrian', phone: '9999999999', email: 'adrian@bitmakerlabs.com',user_id:1, category: 'family', twitter: '', twitter_username: 'BoweiHan')
-Contact.create(name: 'Adrian', phone: '9999999999', email: 'bad',user_id:1, category: 'family', twitter: '1967377642', twitter_username: 'BoweiHan')
-Contact.create(name: 'Adrian', phone: '9999999999', user_id:1, category: 'family', twitter: '1967377642', twitter_username: 'BoweiHan')
+Contact.create(name: 'Ella', phone: '9999999999', email: 'ella@bitmaker.co', user_id:1, category: 'business')
+Contact.create(name: 'Erik', phone: '9999999999', email: 'erik@bitmakerlabs.com' ,user_id:1, category: 'business')
+Contact.create(name: 'Erik Invoices', phone: '9999999999', email: 'erik@bitmaker.co',user_id:1, category: 'business', twitter_username: '')
+Contact.create(name: 'Adrian', phone: '9999999999', email: 'adrian@bitmakerlabs.com',user_id:1, category: 'family')
+Contact.create(name: 'Adrian', phone: '9999999999', email: 'bad',user_id:1, category: 'family')
+Contact.create(name: 'Adrian', phone: '9999999999', user_id:1, category: 'family')
 
 
 
@@ -29,11 +29,11 @@ Contact.create(name: 'Adrian', phone: '9999999999', user_id:1, category: 'family
 #Jon's seeds
 User.create(name: 'test', phone: 'test', email: 'test@test.com', password: 'test', password_confirmation: 'test')
 
-Contact.create(name: 'Codecademy', phone: '9999999999', email: 'contact@codecademy.com', user_id:1, category: 'friend', twitter: '1967377642', twitter_username: 'BoweiHan')
-Contact.create(name: 'Google', phone: '9999999999', email: 'no-reply@accounts.google.com' ,user_id:1, category: 'friend', twitter: '1967377642', twitter_username: 'BoweiHan')
-Contact.create(name: 'Random', phone: '9999999999', email: 'random@random.com',user_id:1, category: 'friend', twitter: '1967377642', twitter_username: 'BoweiHan')
-Contact.create(name: 'Person With no Email', phone: '9999999999',user_id:1, category: 'friend', twitter: '1967377642', twitter_username: 'BoweiHan')
-Contact.create(name: 'Uber', phone: '9999999999', email:"receipts@uber.com", user_id:1, category: 'friend', twitter: '1967377642', twitter_username: 'BoweiHan')
+Contact.create(name: 'Codecademy', phone: '9999999999', email: 'contact@codecademy.com', user_id:1, category: 'friend')
+Contact.create(name: 'Google', phone: '9999999999', email: 'no-reply@accounts.google.com' ,user_id:1, category: 'friend')
+Contact.create(name: 'Random', phone: '9999999999', email: 'random@random.com',user_id:1, category: 'friend')
+Contact.create(name: 'Person With no Email', phone: '9999999999',user_id:1, category: 'friend')
+Contact.create(name: 'Uber', phone: '9999999999', email:"receipts@uber.com", user_id:1, category: 'friend', twitter_username: 'BoweiHan')
 
 #Carol's seeds to seed
 
