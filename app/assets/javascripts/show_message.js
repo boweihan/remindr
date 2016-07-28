ff71358879607b96297199f18a0b88ae3800e82f
bowei <bowei.han100@gmail.com>
2016-07-28 19:50:13 -0400

    message populates

--- app/assets/javascripts/show_message.js
+++ app/assets/javascripts/show_message.js
@@ -1,6 +1,13 @@
 $(document).on('ready', function() {
   $(document).on('click', '.individual_message',  function() {
-    data = $(this).attr('data')
+    var summary = $(this)
+    var data = $(this).attr('data');
+    $(this).css('display','none');
     $('#' + data).css('display', 'block');
+    $(document).on('click', '.individual_message_outer_div',  function() {
+      $(this).css('display', 'none');
+      summary.css('display','block');
+    });
+
   });
 });
