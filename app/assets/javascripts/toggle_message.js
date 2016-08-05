38cefd18f69a956742854c6eccc515cbe6fde9f3
bowei <bowei.han100@gmail.com>
2016-08-05 13:41:35 -0400

    newsfeed meh

--- app/assets/javascripts/toggle_message.js
+++ app/assets/javascripts/toggle_message.js
@@ -1,20 +1,28 @@
 $(document).on('ready', function() {
   $('.gmail-toggle').on('click', function() {
-    $('.gmail-toggle').css('background-color', 'lightgray');
+    $('.gmail-toggle').css('background-color', '#33b4ff');
+    $('.gmail-toggle').css('color', 'white');
     $('.twitter-toggle, .text-toggle').css('background-color', 'white');
+    $('.twitter-toggle, .text-toggle').css('color', 'black');
     $('.send-email-div').css('display','block');
-    $('.send-direct-message').css('display','none');
+    $('.send-direct-message, .send-tweet').css('display','none');
   });
 
   $('.twitter-toggle').on('click', function() {
-    $('.twitter-toggle').css('background-color', 'lightgray');
+    $('.twitter-toggle').css('background-color', '#33b4ff');
+    $('.twitter-toggle').css('color', 'white');
     $('.gmail-toggle, .text-toggle').css('background-color', 'white');
+    $('.gmail-toggle, .text-toggle').css('color', 'black');
     $('.send-direct-message').css('display','block');
-    $('.send-email-div').css('display','none');
+    $('.send-email-div, .send-tweet').css('display','none');
   });
 
   $('.text-toggle').on('click', function() {
-    $('.text-toggle').css('background-color', 'lightgray');
+    $('.text-toggle').css('background-color', '#33b4ff');
+    $('.text-toggle').css('color', 'white');
     $('.gmail-toggle, .twitter-toggle').css('background-color', 'white');
+    $('.gmail-toggle, .twitter-toggle').css('color', 'black');
+    $('.send-tweet').css('display','block');
+    $('.send-email-div, .send-direct-message').css('display','none');
   });
 });
