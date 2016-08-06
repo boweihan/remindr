021e705a3cf4c4f9d20da614f4420ef80bcd2913
bowei <bowei.han100@gmail.com>
2016-08-06 17:09:00 -0400

    commented out something

--- app/assets/javascripts/click_from_reminder.js
+++ app/assets/javascripts/click_from_reminder.js
@@ -1,6 +1,6 @@
-$(document).on('ready', function() {
-  if (window.location.href.split("/")[3].indexOf('newsfeed') > -1) {
-    $('#'+window.location.href.split("#")[1]).click();
-    console.log('hello');
-  }
-})
+// $(document).on('ready', function() {
+//   if (window.location.href.split("/")[3].indexOf('newsfeed') > -1) {
+//     $('#'+window.location.href.split("#")[1]).click();
+//     console.log('hello');
+//   }
+// })
