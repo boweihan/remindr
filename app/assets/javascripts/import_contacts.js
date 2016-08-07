b148a4331c759fc06b0550770806855d205ec7d1
bowei <bowei.han100@gmail.com>
2016-08-07 15:31:07 -0400

    added search and clicking functionality to import contact page

--- app/assets/javascripts/import_contacts.js
+++ app/assets/javascripts/import_contacts.js
@@ -1,27 +1,27 @@
-var contacts = []
-$(function(){
-  $(".potential-contacts").click(function(e){
-    $(this).toggleClass("selected")
-    console.log("been clicked")
-    $('.number_of_selected_contacts').text($(".selected").length)
-  })
-
-  $("#import_contact_button").click(function(e){
-    e.preventDefault()
-
-    $(".selected").each(function(i,item){
-      var info = {}
-      info.name = $(item).find(".potential-contact-name").text()
-      info.phone = $(item).find(".potential-contact-phone-number").val()
-      info.email= $(item).find(".potential-contact-email").text()
-      info.category = 'friend'
-      $.ajax({
-        url:"/contacts",
-        method:"post",
-        data: {contact: info}
-      });
-    });
-
-    window.location.replace("/newsfeed");
-  })
-})
+// var contacts = []
+// $(function(){
+//   $(".potential-contacts").click(function(e){
+//     $(this).toggleClass("selected")
+//     console.log("been clicked")
+//     $('.number_of_selected_contacts').text($(".selected").length)
+//   })
+//
+//   $("#import_contact_button").click(function(e){
+//     e.preventDefault()
+//
+//     $(".selected").each(function(i,item){
+//       var info = {}
+//       info.name = $(item).find(".potential-contact-name").text()
+//       info.phone = $(item).find(".potential-contact-phone-number").val()
+//       info.email= $(item).find(".potential-contact-email").text()
+//       info.category = 'friend'
+//       $.ajax({
+//         url:"/contacts",
+//         method:"post",
+//         data: {contact: info}
+//       });
+//     });
+//
+//     window.location.replace("/newsfeed");
+//   })
+// })
--- app/assets/javascripts/import_contacts_search.js
+++ app/assets/javascripts/import_contacts_search.js
@@ -7,23 +7,54 @@ $(function() {
     }).done(function(response){
       all_contacts = response
       response.forEach(load_imported_contacts);
+      clickable();
     });
 
     $("#search_imported_contacts").on("submit",function(event){
-      event.preventDefault()
+      event.preventDefault();
     });
 
     $(".import-search-bar").keyup(function(){
       var query= $(".import-search-bar").val()
-      var similar = duplicated_elements2(query,all_contacts)
+      var similar = duplicated_elements(query,all_contacts)
       $(".potential-contacts-container").html("")
-      similar.forEach(function(contact){
-        load_imported_contacts(contact)
-      });
+      similar.forEach(load_imported_contacts);
+      clickable();
+      //the code below is busted, @jon let me know if you need it
+      // similar.forEach(function(contact){
+      //   load_imported_contacts(contact)
+      //   clickable();
+      // });
     });
   }
 });
 
+function clickable(){
+
+  $(".potential-contacts").click(function(e){
+    $(this).toggleClass("selected")
+    $('.number_of_selected_contacts').text($(".selected").length)
+  })
+
+  $("#import_contact_button").click(function(e){
+    e.preventDefault()
+
+    $(".selected").each(function(i,item){
+      var info = {}
+      info.name = $(item).find(".potential-contact-name").text()
+      info.phone = $(item).find(".potential-contact-phone-number").val()
+      info.email= $(item).find(".potential-contact-email").text()
+      info.category = 'friend'
+      $.ajax({
+        url:"/contacts",
+        method:"post",
+        data: {contact: info}
+      });
+    });
+
+    window.location.replace("/newsfeed");
+  })
+}
 function load_imported_contacts(contact){
   if (contact.name !== "") {
     $(".potential-contacts-container").append("<div class='potential-contacts'><h2 class='potential-contact-name'>"+contact.name+"</h2><h4 class='potential-contact-email'>"+contact.email+"</h4></div>")
@@ -35,7 +66,7 @@ function load_imported_contacts(contact){
 
 //<input type='hidden' class='potential-contact-phone-number' name='phone_number'>"+contact.phone+"</input>
 
-function duplicated_elements2(searchterm,contactsjson){
+function duplicated_elements(searchterm,contactsjson){
     var names = []
     contactsjson.forEach(function(contact){
       if (isSubstring(searchterm, contact)){
