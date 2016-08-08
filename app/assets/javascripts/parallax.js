a0db72b027bf54e581b2545ec631b9f9b1d9b3e0
bowei <bowei.han100@gmail.com>
2016-08-08 13:59:27 -0400

    landing page css

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,7 +1,6 @@
 $(document).on('ready', function() {
   $(window).on('scroll', function() {
     var position = $(window).scrollTop();
-    console.log($(window).scrollTop());
     console.log($('.photo-background-image').css('margin-top'))
     $('.photo-background-image').css('margin-top', position/2.5)
   });
